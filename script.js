2640630b7d66684b6d55bf50613d8e4796854972
dathuynh99 <68410485+dathuynh99@users.noreply.github.com>
2025-03-28 21:14:37 +0700

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function openSidebar() {
-    document.getElementById("mySidebar").style.transform = "translateX(250px)";
-    document.getElementById("overlay").classList.add("active");
-}
-function closeSidebar() {
-    document.getElementById("mySidebar").style.transform = "translateX(-250px)";
-    document.getElementById("overlay").classList.remove("active");
-}
